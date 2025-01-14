1bc1bf3ad66521b2de70350440afa643a32fdf30
Caio <caioiwakawa@gmail.com>
2025-01-14 10:33:04 -0300

    cycle counter added

--- project_assigner.py
+++ project_assigner.py
@@ -45,9 +45,15 @@ def enrollStudent(project: Project, student: Student, students: list):
         student.match()
         project.enrolledStudents.append(student)
         removed = project.checkAvailability()
+        if removed and removed.code != student.code:
+            print(f"{student.code} foi matriculado em {project.code}")
+            print(f"{removed.code} foi removido de {project.code}")
+        elif not removed:
+            print(f"{student.code} foi matriculado em {project.code}")
         if removed:
             index = students.index(removed)
             students[index].unmatch()
+            
 
 def read_file(filename, students, projects):
     with open(filename, "r") as input:
@@ -71,7 +77,10 @@ def main():
     read_file("input.txt", students, projects)
 
     every_student_matched = False
+    count = 1
     while(not every_student_matched):
+        print(f"CICLO {count}")
+        count += 1
         every_student_matched = True
         for student in students:
             if student.isMatched or student.preference == []:
